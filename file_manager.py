7fdd15ba756f3fe5e759a5962f16319df49fad54
Anastasia Kirillova <a.kirillova.fmedia@gmail.com>
2021-01-19 16:13:41 +0300

    Запись данных из двух файлов в третий

---
+++ file_manager.py
@@ -0,0 +1,29 @@
+from json import loads, dumps
+from csv import DictReader
+
+with open("./files/users.json", "r") as f:
+    users = loads(f.read())
+
+with open("./files/books.csv", "r") as f:
+    reader = DictReader(f)
+
+    lib = []
+
+    for user in users:
+        lib_user = {'name': user["name"],
+                    'gender': user["gender"],
+                    'address': user["address"],
+                    }
+        book = next(reader)
+        lib_book = {"books": [{"title": book["Title"],
+                               "author": book["Author"],
+                               "height": book["Height"],
+                               }]
+                    }
+
+        lib_user.update(lib_book)
+        lib.append(lib_user)
+    print(lib)
+
+with open("./files/result.json", "w") as f:
+    f.write(dumps(lib))
